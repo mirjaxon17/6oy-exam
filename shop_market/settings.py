ce2ac67a5492a39e2f21d77681e587c78627de6c
Aliyev Mirjaxon <mirjaxonaliyev114@gmail.com>
2024-04-25 21:17:15 +0300

    static_root

--- shop_market/settings.py
+++ shop_market/settings.py
@@ -129,14 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = 'media/'
-MEDIAFILES_DIRS = [
-    BASE_DIR / "media",
-]
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
