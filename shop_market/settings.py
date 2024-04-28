719d8acb06252c23ed8b7c3f4866954778d28ab3
Aliyev Mirjaxon <mirjaxonaliyev114@gmail.com>
2024-04-28 18:57:24 +0300

    settings update

--- shop_market/settings.py
+++ shop_market/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['159.65.63.215']
+ALLOWED_HOSTS = []
 
 
 LOGIN_URL = "login"
@@ -129,14 +129,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'  # Выделенная папка для собранных файлов
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',  # Статические файлы вашего приложения
+    BASE_DIR / 'static',
 ]
 
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
+MEDIAFILES_ROOT = [
+    BASE_DIR / 'media',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
